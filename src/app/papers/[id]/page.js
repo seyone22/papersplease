01c2769e8b43656a50ad628084bf888d35fd77f0
seyone <s.g.seyone@protonmail.com>
2023-07-14 13:24:30 +0530

    Updated frontend to match new schema

--- src/app/papers/[id]/page.js
+++ src/app/papers/[id]/page.js
@@ -4,35 +4,35 @@ import { fetchPaperById } from 'utils/database'
 
 import TopNav from 'components/TopNav'
 import QuestionExpander from "../../../../components/QuestionExpander";
-export default async function PaperDetail({params}) {
-
-let currentPaper = await getData(params.id);
+export default async function PaperDetail({ params }) {
+    let currentPaper = await getData(params.id);
 
     return (
         <main className={styles.main}>
-
             <TopNav />
 
             <div className={styles.center}>
                 <h2>{currentPaper.paperYear}</h2>
                 <h1>{currentPaper.paperName}</h1>
             </div>
+
             <div className={styles.grid}>
-                {
-                    currentPaper.questions.map((question, index) => (
-                        <div className={styles.card} key={index}>
-                            <span className={styles.partNumber}> {question.partNumber}. </span>
-                            <span className={styles.questionBody}>{question.questionBody}</span>
-                            <span className={styles.marks}>[{question.marks}%]</span>
-                        </div>
-                    ))
-                }
+                {currentPaper.questions.map((question, questionIndex) => (
+                    <div className={styles.card} key={questionIndex}>
+                        <span className={styles.partNumber}>{question.questionNumber}. </span>
 
+                        {question.parts.map((part, partIndex) => (
+                            <div className={styles.card} key={partIndex}>
+                                {part.partNumber}. {part.questionBody}
+                            </div>
+                        ))}
+                    </div>
+                ))}
             </div>
-
         </main>
-    )
+    );
 }
+
 
 async function getData(id) {
     let paper = await fetchPaperById(id);
