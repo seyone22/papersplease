125c1b4b5902f4d49dabb3525e15a239c7e8860f
seyone <s.g.seyone@protonmail.com>
2023-07-19 16:28:19 +0530

    Implemented Sonali's DELETE function. Modified to use NextRequest properly.

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -55,3 +55,59 @@ export async function GET(req) {
         }
     }
 }
+
+export async function PUT(req) {
+    try {
+        console.log("CONNECTING TO MONGO");
+        await connectMongo();
+        console.log("CONNECTED TO MONGO");
+
+        console.log('UPDATING PAPER');
+
+        const paperId = req.params.id; // Assuming you are passing the paper ID in the URL
+        let updatedPaper = await req.json();
+
+        // Validate that updatedPaper has some data before proceeding with the update
+        if (!updatedPaper || Object.keys(updatedPaper).length === 0) {
+            throw new Error("Invalid request data");
+        }
+
+        // Update the paper with the new data
+        const paper = await Paper.findByIdAndUpdate(paperId, updatedPaper, { new: true });
+
+        if (!paper) {
+            throw new Error("Paper not found");
+        }
+
+        console.log("UPDATED PAPER");
+
+        return NextResponse.json({ updatedPaper });
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error });
+    }
+}
+
+export async function DELETE(req) {
+    try {
+        console.log("CONNECTING TO MONGO");
+        await connectMongo();
+        console.log("CONNECTED TO MONGO");
+
+        const paperId = await req.nextUrl.searchParams.get("id"); // Assuming you are passing the paper ID in the URL
+        const deletedPaper = await Paper.findByIdAndDelete(new mongoose.Types.ObjectId(paperId));
+
+        if (!deletedPaper) {
+            throw new Error("Paper not found");
+        }
+
+        console.log("DELETED PAPER");
+
+        return NextResponse.json({ deletedPaper });
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error });
+    }
+}
