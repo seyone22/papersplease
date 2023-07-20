ae6d1ddad1c16d5adc42b0b72d43b2b2a2e06db1
seyone <s.g.seyone@protonmail.com>
2023-07-20 16:56:39 +0530

    Fixed about page css linkage, added details to question detail page.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,30 +1,37 @@
-import { NextResponse } from 'next/server'
-import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+import TopNav from 'components/TopNav'
 
-const About = () => {
+export default async function PaperDetail({ params }) {
     const creators = [
         { name: 'S.G.Seyone', email: 's.g.seyone@live.com' },
         { name: 'M.G.Sonali.K.Jayarathne', email: 'sonalikalpani@outlook.com' },
     ];
 
     return (
-        <div>
-            <h1>University of Vavuniya Exam Hub</h1>
-            <h2>About</h2>
-            <p>
-                The University of Vavuniya Exam Hub is a platform dedicated to storing, retrieving, viewing, and updating past paper questions and answers. It is designed and developed by 2nd-year ICT students, S.G.Seyone (2019/ICT/46) and M.G.Sonali.K.Jayarathne (2019/ICT/108).
-            </p>
-            <h2>Creators</h2>
-            <ul>
-                {creators.map((creator) => (
-                    <li key={creator.name}>
-                        {creator.name}: <a href={`mailto:${creator.email}`}>{creator.email}</a>
-                    </li>
-                ))}
-            </ul>
+        <main className={styles.main}>
+            <TopNav />
 
-        </div>
+            <div>
+                <h1>University of Vavuniya Exam Hub</h1>
+                <h2>About</h2>
+                <p>
+                    The University of Vavuniya Exam Hub is a platform dedicated to storing, retrieving, viewing, and updating past paper questions and answers. It is designed and developed by 2nd-year ICT students, S.G.Seyone (2019/ICT/46) and M.G.Sonali.K.Jayarathne (2019/ICT/108).
+                </p>
+                <h2>Creators</h2>
+                <ul>
+                    {creators.map((creator) => (
+                        <li key={creator.name}>
+                            {creator.name}: <a href={`mailto:${creator.email}`}>{creator.email}</a>
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
+        </main>
     );
-};
+}
 
-export default About;
+async function getData(id) {
+    let paper = await fetchPaperById(id);
+    return paper;
+}
--- src/app/papers/[id]/[questionNumber]/[partNumber]/page.js
+++ src/app/papers/[id]/[questionNumber]/[partNumber]/page.js
@@ -1,4 +1,13 @@
+import styles from './page.module.css'
+import TopNav from "../../../../../../components/TopNav";
+
 export default async function QuestionDetail({ params }) {
-    console.log(await params);
     //based on the params (id question number and part number), we can generate the page. Here, we will have to create the answers and discussion databases as well.
+
+    return (
+        <main className={styles.main}>
+            <TopNav />
+            {JSON.stringify(params)}
+        </main>
+    )
 }
