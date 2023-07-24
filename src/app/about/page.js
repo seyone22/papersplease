e2e31a84d85349326981b6ae1f3b67681a86b054
MGSK-J <83862174+MGSK-J@users.noreply.github.com>
2023-07-24 21:54:56 +0530

    required database schemas for answer page
    includes database schemas for answers and users

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,6 +5,7 @@ const About = () => {
     const creators = [
         { name: 'S.G.Seyone', email: 's.g.seyone@live.com' },
         { name: 'M.G.Sonali.K.Jayarathne', email: 'sonalikalpani@outlook.com' },
+		{ name: 'Menakan Vijayanathan', email: 'kingshipmena@gmail.com'}
     ];
 
     return (
@@ -12,7 +13,7 @@ const About = () => {
             <h1>University of Vavuniya Exam Hub</h1>
             <h2>About</h2>
             <p>
-                The University of Vavuniya Exam Hub is a platform dedicated to storing, retrieving, viewing, and updating past paper questions and answers. It is designed and developed by 2nd-year ICT students, S.G.Seyone (2019/ICT/46) and M.G.Sonali.K.Jayarathne (2019/ICT/108).
+                The University of Vavuniya Exam Hub is a platform dedicated to storing, retrieving, viewing, and updating past paper questions and answers. It is designed and developed by 2nd-year ICT students, S.G.Seyone (2019/ICT/46) M.G.Sonali.K.Jayarathne (2019/ICT/108) and Menakan Vijayanathan (2019/ICT/45).
             </p>
             <h2>Creators</h2>
             <ul>
