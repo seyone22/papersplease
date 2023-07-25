b6cba08b6d8b452102ab0e274d812fb0a8f6000d
seyone <s.g.seyone@protonmail.com>
2023-07-25 14:26:09 +0530

    Added some CSS to AnswerCard.js, based on the .card class.
    Signed-off-by: seyone <s.g.seyone@protonmail.com>

--- components/Answers/AnswerCard.js
+++ components/Answers/AnswerCard.js
@@ -1,14 +1,17 @@
-'use client'
-import React from "react";
+import styles from './AnswerCard.module.css'
 
 const AnswerCard = ({answer}) => {
+    console.log(answer);
     return (
-        <li key={answer._id}>
-            <p>{answer.answerText}</p>
-            <p>Votes: {answer.votePositive}</p>
-            {/* Displaying 'Superuser Upvoted' if the answer was upvoted by a superuser */}
-            {answer.superVotesPositive && <p>Superuser Upvoted</p>}
-        </li>
-    )
-}
+        <div className={styles.card}>
+            <li key={answer._id} className={styles.answerList}>
+                <p>{answer.answerText}</p>
+                <p>Votes: {answer.votePositive}</p>
+                {/* Displaying 'Superuser Upvoted' if the answer was upvoted by a superuser */}
+                {answer.superVotesPositive && <p>Superuser Upvoted</p>}
+            </li>
+        </div>
+    );
+};
+
 export default AnswerCard;
--- src/app/papers/[id]/[questionNumber]/[partNumber]/page.js
+++ src/app/papers/[id]/[questionNumber]/[partNumber]/page.js
@@ -2,6 +2,7 @@ import styles from './page.module.css'
 import TopNav from "../../../../../../components/TopNav";
 import {fetchPaperById} from "../../../../../../utils/database/paperUtil";
 import {fetchAnswersforQuestionbyId} from "../../../../../../utils/database/answerUtil";
+import AnswerCard from "../../../../../../components/Answers/AnswerCard";
 
 export default async function QuestionDetail({params}) {
     //based on the params (id question number and part number), we can generate the page. Here, we will have to create the Answers and discussion databases as well.
@@ -17,7 +18,9 @@ export default async function QuestionDetail({params}) {
             <div>
                 {
                     tmp.map((answer, answerIndex) => (
-                        <div key={answerIndex}> {answer.answerText} </div>
+                        <div key={answerIndex} className={styles.grid}>
+                            <AnswerCard answer={answer}/>
+                        </div>
                     ))
                 }
             </div>
