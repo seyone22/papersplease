666fd38ce628fbc4705dc2f3d5dab4a21e40a04c
seyone <s.g.seyone@protonmail.com>
2023-07-25 22:01:25 +0530

    Fixed add.js. Purpose of file unknown.
    Signed-off-by: seyone <s.g.seyone@protonmail.com>

--- src/helpers/api/paper/add.js
+++
@@ -1,6 +0,0 @@
-https://jasonwatmore.com/post/2021/08/19/next-js-11-user-registration-and-login-tutorial-with-example-appimport connectMongo from '/utils/connectMongo';
-    import
-Paper
-from
-'models/Paper';
-
