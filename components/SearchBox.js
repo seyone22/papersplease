7fbb63418b7a7af3d3ff8e3c0a2c2ddc47a2543b
seyone <s.g.seyone@protonmail.com>
2023-09-29 15:51:32 +0530

    Changed search button svg to work in dark mmode.
    Signed-off-by: seyone <s.g.seyone@protonmail.com>

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,7 +1,7 @@
 'use client'
 
 import styles from './SearchBox.module.css'
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 
 const SearchBox=() => {
     const router = useRouter();
@@ -20,7 +20,7 @@ const SearchBox=() => {
                     <button className={styles.searchButton} type={"submit"}>
                     <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g clip-path="url(#clip0_15_152)">
-                            <circle cx="10.5" cy="10.5" r="6.5" stroke="#000000" stroke-linejoin="round"/>
+                            <circle cx="10.5" cy="10.5" r="6.5" stroke-linejoin="round"/>
                             <path d="M19.6464 20.3536C19.8417 20.5488 20.1583 20.5488 20.3536 20.3536C20.5488 20.1583 20.5488 19.8417 20.3536 19.6464L19.6464 20.3536ZM20.3536 19.6464L15.3536 14.6464L14.6464 15.3536L19.6464 20.3536L20.3536 19.6464Z" fill="#000000"/>
                         </g>
                         <defs>
